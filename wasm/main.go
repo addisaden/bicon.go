99af9a8ecc632958b8360e90fea326baf2ff41dd
Adrian Enns <adria.aden@gmail.com>
2024-08-24 06:51:35 +0200

    simple print data from base

--- wasm/main.go
+++ wasm/main.go
@@ -6,9 +6,21 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+
+	"github.com/addisaden/bicon.go/base"
 )
 
 func main() {
-	fmt.Println("Hello, WebAssembly")
+	first_book, err := base.GetBookName(1)
+	result := "Hi"
+	g1 := base.GetBereanInterlinear1()
+	g11 := base.Analyze(g1["1_1_1"])
+	fmt.Printf("%v\n", g11)
+	if err == nil {
+		result = fmt.Sprintf("Hello, WebAssembly: %v", first_book)
+	} else {
+		result = fmt.Sprintf("Hello, WebAssembly: Error %v", err)
+	}
+	fmt.Println(result)
 	js.Global().Get("console").Call("log", "Hello from go :) Jesus is King!")
 }
